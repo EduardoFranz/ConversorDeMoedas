26eb0042ed8c2b9e8a4e900499cdbca8c316a6db
Eduardo <eduardo.franz77@gmail.com>
2022-03-10 15:28:17 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function Converter() {
-  var valorElemento = document.getElementById('valor')
-  var valor = valorElemento.value
+  var valorInformado = document.getElementById('valor')
+  var valor = valorInformado.value
+
+  //valor em dólar
   var valorEmDolarNumerico = parseFloat(valor)
+  var valorEmDolar = valorEmDolarNumerico * 5.0
+  console.log(valorEmDolar)
 
-  var valorEmReal = valorEmDolarNumerico * 5
-  console.log(valorEmReal)
+  var elementoValorConvertido = document.getElementById(
+    'valorConvertidoEmDolar'
+  )
+  var valorConvertidoEmDolar =
+    'O valor em dólar é: US$' + valorEmDolar.toFixed(2)
+  elementoValorConvertido.innerHTML = valorConvertidoEmDolar
 
-  var elementoValorConvertido = document.getElementById('valorConvertido')
+  //valor em euro
 
-  var valorConvertido = 'O resultado em real é: R$ ' + valorEmReal
-  elementoValorConvertido.innerHTML = valorConvertido
+  var valorEmEuroNumerico = parseFloat(valor)
+  var valorEmEuro = valorEmEuroNumerico * 5.5
+  console.log(valorEmEuro)
+
+  var elementoValorConvertidoEuro = document.getElementById(
+    'valorConvertidoEmEuro'
+  )
+  var valorConvertidoEmEuro = 'O valor em euro é: €' + valorEmEuro.toFixed(2)
+  elementoValorConvertidoEuro.innerHTML = valorConvertidoEmEuro
 }
